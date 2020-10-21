46e16a867f842b5e70d806d83e3fe471ec3093c2
sharip.nurseit <sharip.nurseit@inessoft.kz>
2020-10-21 16:30:07 +0600

    heroku test

--- src/main.settings/App.js
+++ src/main.settings/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import routes from "../main.settings/routes"
 import Home from "../pages/Home"
 import About from "../pages/About"
 import Navbar from "../components/Navbar";
